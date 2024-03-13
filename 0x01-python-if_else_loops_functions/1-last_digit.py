5693dea9b17a6aa75836cf2491279d03fa0e466e
Benedict <bobuzytech@gmail.com>
2024-03-12 20:46:03 -0700

    Reduce variable name length to suit pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	remainder = number % -10
+    rem = number % -10
 elif number > 0:
-	remainder = number % 10
-if remainder > 5:
-	print(f"Last digit of {number} is {remainder} and is greater than 5")
+    rem = number % 10
+if rem > 5:
+    print(f"Last digit of {number} is {rem} and is greater than 5")
 elif numder == 0:
-	print(f"Last digit of {number} is {remainder} and is 0")
+    print(f"Last digit of {number} is {rem} and is 0")
 else:
-	print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {rem} and is less than 6 and not 0")
