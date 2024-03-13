5ef9172b2137d54948d3fb2a5b87af8e8cffaca9
Benedict <bobuzytech@gmail.com>
2024-03-12 21:05:20 -0700

    Print all alpahbets in lowercase except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for c in range('a', 'z' + 1):
-    if (c == 'e') or (c == 'q'):
+for c in range(97, 123):
+    if (c == 101) or (c == 113):
         continue
     else:
-        print(f')
+        print(chr(c), end="")
