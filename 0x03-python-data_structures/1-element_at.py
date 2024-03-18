859eb878df812504817c1c9bc927f0b81ae12f19
Benedict <bobuzytech@gmail.com>
2024-03-18 14:45:38 -0700

    Remove lenght definition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    lenght = len(my_list)
-    if idx >= length or idx < 0:
+    if (idx >= len(my_list)) or (idx < 0):
         return None
     else:
         return my_list[idx]
