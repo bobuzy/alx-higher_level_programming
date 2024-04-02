eee7a0b395a2a203655ee9b34120dc1c7b93e6be
Benedict <bobuzytech@gmail.com>
2024-04-02 11:43:18 -0700

    Return original x input if no exception occured

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for ind in range(x):
             print(my_list[ind], end="")
         print()
-        return ind + 1
+        return x
     except IndexError:
         print()
         return ind
