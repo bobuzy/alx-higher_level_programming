86cd9512fd84245870f307e3b90066ce72ec3512
Benedict <bobuzytech@gmail.com>
2024-04-03 15:34:56 -0700

    Raise exception if size input is ineligible

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for square class definition"""
+
+
+class Square:
+    """Square class definition"""
+
+    def __init__(self, size=0):
+        """"Initialize attribute size"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
