f7966f6c41fb27f89bf13390eb786244bb501a9f
Benedict <bobuzytech@gmail.com>
2024-04-03 15:27:30 -0700

    Initialize private attribute size in class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for square class definition"""
+
+
+class Square:
+    """Square class definition"""
+    def __init__(self, size):
+        """"Initialize attribute size"""
+        self.__size = size
