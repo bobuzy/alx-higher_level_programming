c0056d3c430e8258817f4ebc605e19f046c6c190
bobuzy <115162603+bobuzy@users.noreply.github.com>
2024-04-11 01:05:24 +0100

    Delete 0x08-python-more_classe directory

--- 0x08-python-more_classe/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""module definition for Rectangle class"""
-
-
-class Rectangle:
-    """Empty Rectangle class definition"""
-    pass
--- 0x08-python-more_classe/README.md
+++
@@ -1,1 +0,0 @@
-I will be solving tasks on 0x08 Python - More Classes and Objects in this directory
