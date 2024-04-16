57ce112928f6b10fdcfe591b597376c778bc4be8
Benedict <bobuzytech@gmail.com>
2024-04-16 03:37:04 -0700

    Validate the name and value attributes

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""BaseGeometry class definition module"""
+
+
+class BaseGeometry:
+    """Raise exception in area method"""
+
+    def area(self):
+        """Raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Check validity of name and value
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value = 0:
+            raise ValueError("{} must be greater than 0".format(name))
