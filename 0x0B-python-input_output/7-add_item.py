2f4a0f8b049ae79bb02a4f68ba726c21ff923677
Benedict <bobuzytech@gmail.com>
2024-04-16 16:51:56 -0700

    Add all arguments to a list and save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module test"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+arglst = list(sys.argv[1:])
+
+try:
+    existing_data = load_from_json_file('add_item.json')
+except Exception:
+    existing_data = []
+
+existing_data.extend(arglst)
+save_to_json_file(existing_data, 'add_item.json')
