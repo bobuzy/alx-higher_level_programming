9a6781d7e131f6e5b52d5878caab875705fff465
Benedict <bobuzytech@gmail.com>
2024-04-16 15:01:20 -0700

    Read file and print to standard output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read_file function definition module"""
+
+
+def read_file(filename=""):
+    """Read file and print contents to standard output"""
+    with open(filename, mode="r",encoding="utf-8") as file:
+        print(file.read(), end="")
