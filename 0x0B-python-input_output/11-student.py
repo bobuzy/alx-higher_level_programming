9b7fd300f2ebb4525956f9a611831ccee8ef0d6d
Benedict <bobuzytech@gmail.com>
2024-04-16 17:13:16 -0700

    Replace all attributes of Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student class definition module"""
+
+
+class Student:
+    """Class definition for Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        if not isinstance(attrs, list):
+            return self.__dict__
+        pack = dict()
+        for attr in attrs:
+            if not isinstance(attr, str):
+                return self.__dict__
+            if attr in self.__dict__:
+                pack[attr] = self.__dict__[attr]
+        return pack
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
