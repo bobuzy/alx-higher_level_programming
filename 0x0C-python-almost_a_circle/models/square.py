d7009d9cc4ad6054f2907d2249b5e6f976404610
Benedict <bobuzytech@gmail.com>
2024-04-22 08:49:40 -0700

    Create square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Square class definition module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class definition"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize the Square instance using the Rectangle class"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Print the basic information fo the square instance"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id,
+                self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """Return the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the width and height attribute with a new value"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the attributes using args and kwargs"""
+        if args:
+            for ind in range(len(args)):
+                if ind == 0:
+                    self.id = args[ind]
+                elif ind == 1:
+                    self.size = args[ind]
+                elif ind == 2:
+                    self.x = args[ind]
+                elif ind == 3:
+                    self.y = args[ind]
+                else:
+                    raise TypeError("update() takes at most 4 arguments " +
+                                    "but more than 4 was given")
+        elif kwargs:
+            self.id = kwargs.get('id', self.id)
+            self.size = kwargs.get('size', self.width)
+            self.x = kwargs.get('x', self.x)
+            self.y = kwargs.get('y', self.y)
