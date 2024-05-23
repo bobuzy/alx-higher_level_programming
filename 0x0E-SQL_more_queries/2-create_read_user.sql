ba23910366a0c1b19e27e696bd34307318498384
Benedict <bobuzytech@gmail.com>
2024-05-23 21:27:20 +0000

    Quote username

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- Create new database and user, and grant SELECT priviledges on the new database
--- to the new user only
+-- Create new database and user, and grant SELECT priviledges on the new database to the new user only
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd; 
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd'; 
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
