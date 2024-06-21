b7b67b3515ad7ae3895218e3dd8f04776f7ffcae
Benedict <bobuzytech@gmail.com>
2024-06-21 08:14:48 +0000

    Change 'Cool' to 'cool' for correct output

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const strings = ['C is fun', 'Python is Cool', 'JavaScript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const line of strings) {
   console.log(line);
 }
