3206386d74675eda864cc6056228f434db3ed907
Benedict <bobuzytech@gmail.com>
2024-06-23 16:34:34 +0000

    Use space indents instead of tab

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,27 +5,27 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-	try:
-		db = MySQLdb.connect(
-			host="localhost",
-			port=3306,
-			user=username,
-			passwd=password,
-			db=database)
-	except MySQLdb.ERROR as error:
-		print("Error connecting to database: {}".format(error))
-		sys.exit(1)
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
+    except MySQLdb.ERROR as error:
+        print("Error connecting to database: {}".format(error))
+        sys.exit(1)
 
-	curr = db.cursor()
+        curr = db.cursor()
 
-	curr.execute("SELECT * FROM states ORDER BY id ASC")
-	rows = curr.fetchall()
+        curr.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = curr.fetchall()
 
-	for row in rows:
-		print(row)
-	curr.close()
-	db.close()
+        for row in rows:
+            print(row)
+        curr.close()
+        db.close()
