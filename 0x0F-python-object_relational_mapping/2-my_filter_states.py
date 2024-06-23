4c173c793d497dc39f1f930d15b7cbde4b1e0299
Benedict <bobuzytech@gmail.com>
2024-06-23 21:27:15 +0000

    Filter state by search imput

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module definition to lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    keyword = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=username,
+                             passwd=password,
+                             db=database)
+    except MySQLdb.ERROR as error:
+        print("Error connecting to database: {}".format(error))
+        sys.exit(1)
+
+    curr = db.cursor()
+
+    curr.execute("SELECT * FROM states WHERE name \
+                 LIKE BINARY '{}' ORDER BY id ASC".format(keyword))
+    rows = curr.fetchall()
+
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()
