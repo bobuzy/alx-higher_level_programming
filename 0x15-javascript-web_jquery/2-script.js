29e80722199557ff435e305e23e95fb9728d2eb8
bobuzy <bobuzytech@gmail.com>
2024-08-09 19:26:56 +0000

    Update the color of the header element on red_header div click

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Updates the text color of the <header> element to red
+// when a user clicks on the red_reader div element
+$('DIV#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
